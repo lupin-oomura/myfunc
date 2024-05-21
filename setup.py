96ce7d7fa4623082ac11b280b673ffaece4efbf3
lupin-oomura <112717283+lupin-oomura@users.noreply.github.com>
2024-05-21 21:34:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='myfunc',
     version='0.1',
     packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/lupin-oomura/myfunc.git',
+    author='Your Name',
+    author_email='shin.oomura@gmail.com',
+    description='A simple OpenAI function package',
 )
